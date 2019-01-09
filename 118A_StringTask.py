cd9064220aea3be25203be1b096091b9507836da
harpreetvirkk <harpreetvirk2018@gmail.com>
2019-01-09 17:27:00 +0530

    158A

---
+++ 118A_StringTask.py
@@ -0,0 +1,8 @@
+#https://codeforces.com/problemset/problem/118/A
+string = input()
+string = string.lower()
+new = []
+for i in range(len(string)):
+    if string[i] != 'a' and string[i] !='e' and string[i] !='i' and string[i] != 'o' and string[i] !='u' and string[i] !='y':
+        new.append(string[i])
+print("."+".".join(new))
