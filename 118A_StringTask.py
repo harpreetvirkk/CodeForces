07bb2d1c429925e8e1693418858361bbe75aad70
Harpreet Virk <39559047+harpreetvirkk@users.noreply.github.com>
2019-01-09 17:30:20 +0530

    118A

--- 118A_StringTask.py
+++ 118A_StringTask.py
@@ -6,4 +6,3 @@ for i in range(len(string)):
     if string[i] != 'a' and string[i] !='e' and string[i] !='i' and string[i] != 'o' and string[i] !='u' and string[i] !='y':
         new.append(string[i])
 print("."+".".join(new))
-a
