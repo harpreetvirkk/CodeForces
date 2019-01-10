162ae29110cce315beeb3cf7585780936eb9d24d
harpreetvirkk <harpreetvirk2018@gmail.com>
2019-01-10 23:18:43 +0530

    solutions

--- 705A_Hulk.py
+++ 705A_Hulk.py
@@ -1,13 +1,21 @@
+#https://codeforces.com/problemset/problem/705/A
 n=int(input())
 flag=0
+sentence=[]
 if n==1:
-    print("I hate it")
+    sentence.append("I hate it")
 else:
     for i in range(n):
         if flag==0:
-            print("I hate "),
+            sentence.append("I hate that ")
             flag=1
         else:
-            print("I love "),
+            sentence.append("I love that ")
             flag=0
-        print("that ")
+    if flag==1:
+        sentence.pop(n-1)
+        sentence.append("I hate it")
+    else:
+        sentence.pop(n-1)
+        sentence.append("I love it")
+print("".join(sentence))
